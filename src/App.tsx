825281e9042e3748e3d96da36d21e16f1d7487a0
Joshua Maier <joshuam@udel.edu>
2025-09-07 23:17:34 -0400

    Added the updated code for task solved-html-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./App.css";
+import img from "./assets/cello-recital.jpg";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App(): React.JSX.Element {
     return (
@@ -11,8 +16,57 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
             <p>Joshua Maier</p>
-            <h1>Hello World</h1>
+
+            <h2>Hello World</h2>
+
+            <Container fluid="md">
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "200px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                        First Column. Top Three Animes:
+                        <ol>
+                            <li>Attack on Titan</li>
+                            <li>One Piece</li>
+                            <li>Full Metal Alchemist: Brotherhood</li>
+                        </ol>
+                    </Col>
+
+                    <Col>
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "200px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                        Second Column.
+                        <img
+                            src={img}
+                            alt="Me getting ready for a cello recital"
+                            width="300"
+                            height="400"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+
+            <div>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+            </div>
         </div>
     );
 }
